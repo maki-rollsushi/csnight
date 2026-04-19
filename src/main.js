e2c5ae0a5f6f73c11ec5b79c4cd3256f1159d180
maki-rollsushi <delacruzmacky39@gmail.com>
2026-04-19 22:49:04 +0800

    asfsfd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./style.css";
 import { initScanner } from "./scanner.js";
 import { renderApp } from "./app.js";
-import { uploadGuestsFromCSV } from "./firebase.js";
 
 renderApp();
 initScanner();
